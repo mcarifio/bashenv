a3a37752f105fa3ef75a1a7434fd09f7a92d67fa
Mike Carifio <mcarifio@ciq.com>
2024-02-14 09:58:05 -0500

    update README.md, add pj directory

--- .bash_profile
+++ .bash_profile
@@ -3,8 +3,6 @@
 source $(realpath ${BASH_SOURCE}).lib.sh || true
 export PATH="$(path.login):${PATH}"
 source.bash_profile.d
-source.bashrc.d
-source.bash_completion.d
 
 # User specific aliases and functions
 [[ -r ~/.bashrc ]] && source ~/.bashrc
--- .bash_profile.lib.sh
+++ .bash_profile.lib.sh
@@ -1,9 +1,62 @@
-home() ( getent passwd ${SUDO_USER:-${USER}} | cut -d: -f6; ); declare -fx home
+home() (
+    : 'home [${user}] #> the login directory of the optional user.'
+    getent passwd ${1:-${SUDO_USER:-${USER}}} | cut -d: -f6
+); declare -fx home
+
+# Return the full pathname of the bashenv root directory, usually something like ${HOME}/bashenv.
+# Depends on where you placed it however.
+eval "bashenv.root() ( echo $(dirname $(realpath ${BASH_SOURCE})); )"; declare -fx bashenv.root
 
 path.login() (
+    : 'path.login will return interesting directories under ${HOME}'
     printf '%s:' $(home)/opt/*/current/bin $(home)/.config/*/bin
 ); declare -fx path.login
 
+path.add() {
+    : 'path.add ${folder} [after] ## adds ${folder} to PATH iff not already there'
+    case ":${PATH}:" in
+        *:"$1":*)
+            ;;
+        *)
+            if [ "$2" = "after" ] ; then
+                PATH=$PATH:$1
+            else
+                PATH=$1:$PATH
+            fi
+    esac
+}; declare -fx path.add
+
+
+path.walk() (
+    : 'path.walk ${folder} [${min} [${max}]] #> all directories under ${folder}'
+    local -r _root=${1:-${PWD}}
+    local -ri _maxdepth=${2:-1}
+    local -ri _mindepth=${3:-1}
+    find . -mindepth ${_mindepth} -maxdepth ${_maxdepth} -type d -regex '.*/[^\.]+$'
+); declare -fx path.walk
+
+path.pn1() ( realpath -Lms ${1:-${PWD}}; ); declare -fx path.pn1
+path.pn() ( _map path.pn1 $* ; ); declare -fx path.pn
+# full pathname 1
+path.fpn1() ( echo -n ${HOSTNAME}:; realpath -Lms ${1:-${PWD}}; ); declare -fx path.fpn1
+# full pathname
+path.fpn() ( _map path.fpn1 $* ; ); declare -fx path.fpn
+
+
+path.md() (
+    : 'path.md ## make a directory and return its pathname, e.g cp foo $(path.md /tmp/foo)/bar'
+    local _d=$(path.pn1 $1)
+    [[ -d "$_d" ]] || mkdir -p ${_d}
+    printf "%s" ${_d}
+); declare -fx path.md
+
+path.mkcd() {
+    local _d=$(path.md $1); [[ -z "${_d}" ]] || cd -Pe ${_d}
+}; declare -fx path.mkcd
+
+path.mp() ( local _p=$(printf "%s/%s" $(md $1/..) ${1##*/}); printf ${_p}; ); declare -fx path.mp
+path.mpt() ( local _p=$(printf "%s/%s" $(md $1/..) ${1##*/}); touch ${_p}; printf ${_p}; ); declare -fx path.mpt
+path.mpcd() ( cd $(dirname $(mp ${1:?'expecting a pathname'})); ); declare -fx path.mpcd
 
 source.all() {
     for _a in $@; do
@@ -17,15 +70,15 @@ source.find() {
 }; declare -fx source.find
 
 source.bash_profile.d() {
-    source.find $(home)/.bash_profile.d
+    source.find $(bashenv.root)/.bash_profile.d
 }; declare -fx source.bash_profile.d
 
 source.bashrc.d() {
-    source.find $(home)/.bashrc.d
+    source.find $(bashenv.root)/.bashrc.d
 }; declare -fx source.bashrc.d
 
 source.bash_completion.d() {
-    source.find $(home)/.local/bash_completion.d
+    source.find $(bashenv.root)/.bash_completion.d
 }; declare -fx source.bash_completion.d
 
 running.bash() { realpath /proc/$$/exe | grep -Eq 'bash$' || return 0; }; declare -fx running.bash
