121193313345807a37efc5e0c6a9155c268b88a7
Mike Carifio <mcarifio@ciq.com>
2024-03-04 08:49:22 -0500

    add shim

---
+++ .bash_profile.d/shim.source.sh
@@ -0,0 +1,10 @@
+shim() (
+    local _from=${1:-'expecting a folder'}
+    local _to=${2:-${HOME}}
+    find "${_from}" -type f -exec echo ${_to}/$(realpath  --relative-to=${_from} --strip {}) \;
+)
+__shim.complete() {
+    : ''
+}
+
+f.complete shim # __shim.complete
