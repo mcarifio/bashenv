0b9ac387b9b3e4f2742c90e614e1fbbf2789a11a
Mike Carifio <mcarifio@ciq.com>
2024-03-08 16:59:46 -0500

    do something before shutting down

---
+++ .bash_profile.d/shutdown.source.sh
@@ -0,0 +1,9 @@
+# shutdown in a machine specific way
+shutdown() (
+    for d in ~/.config/keepassxc ~/Documents/e; do
+	rsync.local $d
+    done
+    dnf upgrade -y
+    sudo shutdown -h now    
+)
+f.complete shutdown
