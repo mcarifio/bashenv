a7977aeb4b967d740870a494aa3daf1c36963918
Mike Carifio <mcarifio@ciq.com>
2024-03-11 18:52:41 -0400

    bin/bashenv.sh is the template for .sh scripts.

--- bin/bashenv.sh
+++ bin/bashenv.sh
@@ -10,9 +10,30 @@ doctor() (
 
 start() (
     [[ "${FUNCNAME}" = "$1" ]] && shift
-    echo ${FUNCNAME} "$@"
+    declare -A _flags=( [entry]=${FUNCNAME} [verbose]=0 )
+    declare -a _args=()
+    _parse _flags _args "$@"
+    echo ${_flags[verbose]}
+    echo ${_flags[foo]}
+    echo "${_args[@]}"
 )
 
+
+# all entry points parse arguments first
+_parse() {
+    declare -n _Aref=$1; shift
+    declare -n _aref=$1; shift
+    for _a in "$@"; do
+	case "${_a}" in
+	    --) shift;break;;
+	    --*=*) [[ "${_a}" =~ ^--([^=]+)=(.*)$ ]] && _Aref[${BASH_REMATCH[1]}]="${BASH_REMATCH[1]}";;
+	    --*) [[ "${_a}" =~ ^--(.+)$ ]] && _Aref[${BASH_REMATCH[1]}]=1;;
+	    *) break;;
+	esac
+	shift
+    done
+    _aref=( "$@" )
+}
 
 # dispatch
 ${1:-start} "$@"
