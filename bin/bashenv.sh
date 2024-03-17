1f24b9e49cc8e7779ced24fc0ee81433a27954bc
Mike Carifio <mcarifio@ciq.com>
2024-03-17 19:27:42 -0400

    shfmt

--- bin/bashenv.sh
+++ bin/bashenv.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-
 doctor() (
     [[ "${FUNCNAME}" = "$1" ]] && shift
     echo ${FUNCNAME} "$@"
 )
 
-
-
 start() (
     [[ "${FUNCNAME}" = "$1" ]] && shift
-    declare -A _flags=( [entry]=${FUNCNAME} [verbose]=0 )
+    declare -A _flags=([entry]=${FUNCNAME} [verbose]=0)
     declare -a _args=()
     _parse _flags _args "$@"
     echo ${_flags[verbose]}
@@ -18,21 +15,25 @@ start() (
     echo "${_args[@]}"
 )
 
-
 # all entry points parse arguments first
 _parse() {
-    declare -n _Aref=$1; shift
-    declare -n _aref=$1; shift
+    declare -n _Aref=$1
+    shift
+    declare -n _aref=$1
+    shift
     for _a in "$@"; do
-	case "${_a}" in
-	    --) shift;break;;
-	    --*=*) [[ "${_a}" =~ ^--([^=]+)=(.*)$ ]] && _Aref[${BASH_REMATCH[1]}]="${BASH_REMATCH[1]}";;
-	    --*) [[ "${_a}" =~ ^--(.+)$ ]] && _Aref[${BASH_REMATCH[1]}]=1;;
-	    *) break;;
-	esac
-	shift
+        case "${_a}" in
+        --)
+            shift
+            break
+            ;;
+        --*=*) [[ "${_a}" =~ ^--([^=]+)=(.*)$ ]] && _Aref[${BASH_REMATCH[1]}]="${BASH_REMATCH[1]}" ;;
+        --*) [[ "${_a}" =~ ^--(.+)$ ]] && _Aref[${BASH_REMATCH[1]}]=1 ;;
+        *) break ;;
+        esac
+        shift
     done
-    _aref=( "$@" )
+    _aref=("$@")
 }
 
 # dispatch
