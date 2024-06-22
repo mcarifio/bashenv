175d0eba25101fc48081ca0213953518be834c8b
Mike Carifio <mcarifio@ciq.com>
2024-06-22 10:56:57 -0400

    move install to ${guard}.install.sh; simplify dnf()

--- profile.d/dnf.guard.sh
+++ profile.d/dnf.guard.sh
@@ -5,8 +5,8 @@
 # usage: [guard | source] dnf.guard.sh [--install] [--verbose] [--trace]
 _guard=$(path.basename ${BASH_SOURCE})
 declare -A _option=([install]=0 [verbose]=0 [trace]=0)
-_undo=''; trap -- 'eval ${_undo}; unset _option _undo; trap -- - RETURN' RETURN
-local -a _rest=( $(u.parse _option "$@") )
+declare -a _rest=( $(u.parse _option "$@") )
+_undo=''; trap -- 'eval ${_undo}; unset _option _rest _undo; trap -- - RETURN' RETURN
 if (( ${_option[trace]} )) && ! bashenv.is.tracing; then
     _undo+='set +x;'
     set -x
@@ -14,14 +14,16 @@ fi
 if (( ${_option[install]} )); then
     if u.have ${_guard}; then
         >&2 echo ${_guard} already installed
-    else
+    elif [[ -x "${BASH_SOURCE/.guard./.install.}" ]] ; then
+        "${BASH_SOURCE/.guard./.install.}"
+    else        
         u.bad "${BASH_SOURCE} --install # not implemented"
     fi
 fi
 
 dnf() (
     : "sudo dnf ..."
-    sudo /usr/bin/dnf "$@" -y --allowerasing
+    sudo /usr/bin/dnf --assumeyes "$@"# --allowerasing
 )
 f.x dnf
 
