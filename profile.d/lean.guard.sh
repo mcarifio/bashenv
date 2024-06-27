62aaaff55088e3c091ed8a91b23678632a3acc41
Mike Carifio <mcarifio@ciq.com>
2024-06-27 16:43:13 -0400

    Added lean4 commands and emacs support. Does not yet work.

---
+++ profile.d/lean.guard.sh
@@ -0,0 +1,48 @@
+# usage: [guard | source] template.guard.sh [--install] [--verbose] [--trace]
+
+# Front matter. Parse the source command line. Install by platform if --install,
+# trace (and revert) if --trace.
+_guard=$(path.basename ${BASH_SOURCE})
+declare -A _option=([install]=0 [verbose]=0 [trace]=0)
+declare -a _rest=( $(u.parse _option "$@") )
+_undo=''; trap -- 'eval ${_undo}; unset _option _rest _undo; trap -- - RETURN' RETURN
+if (( ${_option[trace]} )) && ! bashenv.is.tracing; then
+    _undo+='set +x;'
+    set -x
+fi
+if (( ${_option[install]} )); then
+    if u.have ${_guard}; then
+        >&2 echo ${_guard} already installed
+    elif [[ -x "${BASH_SOURCE/.guard./.install.}" ]] ; then
+        "${BASH_SOURCE/.guard./.install.}"
+    else        
+        $(u.here)/guard.install.sh ${BASH_SOURCE%%.*}
+    fi
+fi
+
+# template itself
+# not working
+_template.parse() (
+    : '## example: parse callers arglist'
+    set -uEeo pipefail
+    shopt -s nullglob
+    declare -A _template_options=([file]="${PWD}/${FUNCNAME}" [comment]="${HOSTNAME}:and_some_stuff" [trace]=0)
+    local -a _rest=( $(u.parse _template_options --foo=bar --user=${USER} --trace 1 2 3) )
+    printf '%s ' ${FUNCNAME}; declare -p _template_options; printf '%s ' ${_rest[@]}
+)
+
+# TODO mike@carif.io: logic needs fixing
+f.x _template.parse
+
+
+template.env() {
+    true || return $(u.error "${FUNCNAME} failed")
+}
+f.x template.env
+
+template.session() {
+    true || return $(u.error "${FUNCNAME} failed")
+}
+f.x template.session
+
+loaded "${BASH_SOURCE}"
--- profile.d/lean.install.sh
+++ profile.d/lean.install.sh
@@ -1,5 +1,5 @@
+# _template.install.sh will install _template depending on the distro id.
 #!/usr/bin/env bash
-# use guard.install.sh as the template to start ${item}.install.sh
 
 # brew install will upgrade older versions.
 # https://formulae.brew.sh/formula/elan-init#default
@@ -11,7 +11,7 @@ install.ubuntu() ( sudo /usr/bin/apt install -y "${$@}"; )
 
 # dispatch
 _install() ( install.$(os.release ID) "$@"; )
-install() ( install.$(os.release ID) "$@"; )
+install() ( install.brew elan-init; )
 
 install "$@"
 
