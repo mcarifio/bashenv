f1ddc7b6d861b2be16c77849b8fbe150bd0bb2ce
Mike Carifio <mike@carif.io>
2024-07-29 13:40:27 -0400

    add install.asdf to template

--- profile.d/_template.install.sh
+++ profile.d/_template.install.sh
@@ -6,6 +6,15 @@
 # https://formulae.brew.sh/formula/elan-init#default
 install.brew() ( brew install "$@"; )
 
+install.asdf() (
+    set -Eeou pipefail
+    local _plugin=${1:?'expecting an asdf plugin'}
+    local _version=${2:-latest}
+    asdf plugin add ${_plugin} ${3:-}
+    asdf install ${_plugin} ${_version}
+    asdf global ${_plugin} ${_version}
+)
+
 # by distro id
 install.fedora() ( sudo /usr/bin/dnf install --assumeyes "${$@}"; )
 install.ubuntu() ( sudo /usr/bin/apt install -y "${$@}"; )
@@ -14,5 +23,7 @@ install.ubuntu() ( sudo /usr/bin/apt install -y "${$@}"; )
 # install() ( install.$(os.release ID) "$@"; )
 install() ( install.$(os.release ID) "$@"; )
 
+# install $(path.basename ${BASH_SOURCE}) "$@"
+# install "$@"
 install "$@"
 
