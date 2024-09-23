4215ca5ecb007f73118bdb7d981c6cb8df547c50
Mike Carifio <mike@carif.io>
2024-09-22 20:09:43 -0400

    lib.install.sh -> _defaults.install.sh, better name for default install functions.

--- profile.d/_template.install.sh
+++ profile.d/_template.install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail; shopt -s nullglob
 [[ "$0" = */bashdb ]] && shift
-source ${0%/*}/lib.install.sh
+source ${0%/*}/_defaults.install.sh
 
 _install $(path.basename.part "$0" 1) $(path.basename "$0") "$@"
 
