8c7de4ec9222b2d5709ccfd19c1cdb9acc9fc7cc
Mike Carifio <mcarifio@ciq.com>
2024-09-26 17:48:50 -0400

    add post.install

--- profile.d/binstall.d/rustup.sh.binstall.sh
+++ profile.d/binstall.d/rustup.sh.binstall.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 source $(u.here)/binstalld.lib.sh
-binstalld.dispatch --kind=$(path.basename.part "$0" 1) --pkg=$(path.basename "$0") --url=https://sh.rustup.rs -- -y --no-modify-path --profile default "$@"
+
+post.install() {
+    source ${HOME}/.cargo/env
+}
+
+binstalld.dispatch --kind=$(path.basename.part "$0" 1) --pkg=$(path.basename "$0") --url=https://sh.rustup.rs --postinstall=post.install -- -y --no-modify-path --profile default "$@"
 
 
