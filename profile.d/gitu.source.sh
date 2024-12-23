97899f99b5efefb99983e0c924cce8ef9a50fba1
Mike Carifio <mike@carif.io>
2024-12-22 23:07:49 -0500

    Added cargo gitu

---
+++ profile.d/gitu.source.sh
@@ -0,0 +1,56 @@
+${1:-false} || u.haveP $(path.basename.part ${BASH_SOURCE} 0) || return 0
+
+gitu() ( command ${FUNCNAME} "$@"; )
+f.x gitu
+
+gitu.doc.urls() ( echo ; ) # urls here
+f.x gitu.doc.urls
+
+gitu.doc() (
+    set -Eeuo pipefail; shopt -s nullglob
+    for _u in $(${FUNCNAME}.urls); do xdg-open ${_u}; done
+)
+f.x gitu.doc
+
+gitu.env() {
+    : '# called (once) by .bash_profile'
+    true || return $(u.error "${FUNCNAME} failed")
+}
+f.x gitu.env
+
+gitu.session() {
+    : '# called by .bashrc'
+    local -r _shell=${1:-$(u.shell)}
+    local -r _cmd=${2:-${FUNCNAME%.*}}
+    local -r _completions=/usr/share/bash-completion/completions
+    source.if ${_completions}/${_cmd}.${_shell} ${_completions}/${_cmd}
+}
+f.x gitu.session
+
+gitu.installer() (
+    set -Eeuo pipefail # DO NOT shopt -s nullglob
+    realpath -Lm $(binstall.installer ${FUNCNAME%.*})
+)
+f.x gitu.installer
+
+gitu.config.dir() (
+    set -Eeuo pipefail; shopt -s nullglob
+    local _guard="${FUNCNAME%%.*}"
+    local _dir="${HOME}/.config/${_guard}"
+    [[ -d "${_dir}" ]] || return $(u.error "${FUNCNAME} '${_dir}' not found")
+    echo ${_dir}
+)
+f.x gitu.config.dir
+
+gitu.config() (
+    set -Eeuo pipefail; shopt -s nullglob
+    local _guard="${FUNCNAME%%.*}"
+    local _dir="$(${FUNCNAME}.dir)"
+    local _config="${_dir}/${_guard}"
+    [[ -r "${_config}" ]] || return $(u.error "${FUNCNAME} '${_config}' not found")
+    echo "${_config}"
+)
+f.x gitu.config
+
+
+sourced || true
