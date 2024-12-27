4bec7adb3c21e4f73835149c997a9286756908ea
Mike Carifio <mcarifio@ciq.com>
2024-12-26 21:43:10 -0500

    vivaldia, brave-browser

--- profile.d/binstall.d/brave-browser.dnf.binstall.sh
+++ profile.d/binstall.d/brave-browser.dnf.binstall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 source $(u.here)/binstalld.lib.sh
-binstalld.dispatch --kind=$(path.basename.part "$0" 1) --pkg=$(path.basename "$(realpath -Lm "$0")") "$@"
+binstalld.dispatch --kind=$(path.basename.part "$0" 1) --pkg=$(path.basename "$(realpath -Lm "$0")") \
+		   --import=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc \
+		   --add-repo=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo \
+		   "$@"
 
 
