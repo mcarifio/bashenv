6cb38cd9dbe178ed274bb7f7ec49c7f55449e199
Mike Carifio <mike@carif.io>
2024-12-30 18:26:29 -0500

    Add zed and cursor, editors

--- profile.d/binstall.d/cursor.AppImage.binstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source $(u.here)/binstalld.lib.sh
-
-exit $(u.error "need a url") 1
-
-binstalld.dispatch --kind=$(path.basename.part "$0" 1) --pkg=$(path.basename "$(realpath -Lm "$0")") "$@"
