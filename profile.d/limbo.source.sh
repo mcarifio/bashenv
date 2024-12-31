b0502692a29e6622410e036b5dbc2b588fb0fb6f
Mike Carifio <mike@carif.io>
2024-12-30 21:09:43 -0500

    install limbo

---
+++ profile.d/limbo.source.sh
@@ -0,0 +1,56 @@
+${1:-false} || u.haveP $(path.basename.part ${BASH_SOURCE} 0) || return 0
+
+limbo() ( command ${FUNCNAME} "$@"; )
+f.x limbo
+
+limbo.doc.urls() ( echo ; ) # urls here
+f.x limbo.doc.urls
+
+limbo.doc() (
+    set -Eeuo pipefail; shopt -s nullglob
+    for _u in $(${FUNCNAME}.urls); do xdg-open ${_u}; done
+)
+f.x limbo.doc
+
+limbo.env() {
+    : '# called (once) by .bash_profile'
+    true || return $(u.error "${FUNCNAME} failed")
+}
+f.x limbo.env
+
+limbo.session() {
+    : '# called by .bashrc'
+    local -r _shell=${1:-$(u.shell)}
+    local -r _cmd=${2:-${FUNCNAME%.*}}
+    local -r _completions=/usr/share/bash-completion/completions
+    source.if ${_completions}/${_cmd}.${_shell} ${_completions}/${_cmd}
+}
+f.x limbo.session
+
+limbo.installer() (
+    set -Eeuo pipefail # DO NOT shopt -s nullglob
+    realpath -Lm $(binstall.installer ${FUNCNAME%.*})
+)
+f.x limbo.installer
+
+limbo.config.dir() (
+    set -Eeuo pipefail; shopt -s nullglob
+    local _guard="${FUNCNAME%%.*}"
+    local _dir="${HOME}/.config/${_guard}"
+    [[ -d "${_dir}" ]] || return $(u.error "${FUNCNAME} '${_dir}' not found")
+    echo ${_dir}
+)
+f.x limbo.config.dir
+
+limbo.config() (
+    set -Eeuo pipefail; shopt -s nullglob
+    local _guard="${FUNCNAME%%.*}"
+    local _dir="$(${FUNCNAME}.dir)"
+    local _config="${_dir}/${_guard}"
+    [[ -r "${_config}" ]] || return $(u.error "${FUNCNAME} '${_config}' not found")
+    echo "${_config}"
+)
+f.x limbo.config
+
+
+sourced || true
