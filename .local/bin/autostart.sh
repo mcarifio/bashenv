7b1a18c7e4df0458b417d7a93358a0eca15e146c
Mike Carifio <mike@carif.io>
2024-12-31 12:25:18 -0500

    autostart.sh now sources alacritty.source.sh

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,48 +1,16 @@
 #!/usr/bin/env bash
 # invoked by ~/.config/autostart.desktop
 
-set -uo pipefail
+set -Eeuo pipefail
 
 # Does gnome autostart login first?
 _bashenv=$(bashenv.root 2>/dev/null || echo ${HOME}/bashenv/.bash_profile.d)
-
-sudo.alacritty() (
-    set -Eeuo pipefail; shopt -s nullglob
-    local _title=${FUNCNAME}
-    local -i _lines=40 _columns=200
-
-    for _a in "${@}"; do
-        case "${_a}" in
-            --title=*) _title="${_a##*=}";;
-            --lines=*) _title="${_a##*=}";;
-            --columns=*) _title="${_a##*=}";;
-            --) shift; break;;
-            --*) break;; ## break on unknown switch, pass it along
-            *) break;;
-        esac
-        shift
-    done
-    # sudo -E alacritty --title "${_title}" --option window.dimensions.{lines=50,columns=400} --command "$@" &
-    sudo $(type -P alacritty) --title "${_title}" --option window.dimensions.{lines=${_lines},columns=${_columns}} --command "$@" &
-)
-
-# bashenv/profile.d/binstall.d/showmethekey.dnf.binstall.sh
-watch.input() (
-    sudo.alacritty --title=${FUNCNAME} showmethekey-cli &
-)
-
-
-watch.dmesg() (
-    sudo.alacritty --title=${FUNCNAME} dmesg -HT --color=always --follow &
-)
-
-watch.nvtop() (
-    alacritty --title "${FUNCNAME}" --command nvtop &
-)
-
-watch.input
-watch.dmesg
-watch.nvtop
+if u.have alacritty; then
+    source ${_bashenv}/alacrity.source.sh
+    watch.input
+    watch.dmesg
+    watch.nvtop
+fi
 
 d.run() (
     for _host in "$@"; do
@@ -55,7 +23,7 @@ d.run() (
 # thunderbird &
 
 # gnome-terminal
-desktop.run Terminal
+desktop.run Terminal Terminator Tabby
 
 # slack
 # slack &
@@ -69,13 +37,7 @@ desktop.run Terminal
 
 # can't have too many browsers
 # chrome browser
-desktop.run google-chrome
-# edge browser, flatpak
-desktop.run microsoft-edge
-# chromium
-desktop.run chromium-browser
-# vivaldi
-desktop.run vivaldi
+desktop.run google-chrome chromium-browser microsoft-edge vivaldi brave opera
 # opera
 # opera &
 
