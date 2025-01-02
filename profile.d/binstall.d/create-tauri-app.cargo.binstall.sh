dacbba2a405215045a6bc841bfd20119e6f22331
Mike Carifio <mike@carif.io>
2025-01-01 19:33:29 -0500

    use new cargo template for tauri

--- profile.d/binstall.d/create-tauri-app.cargo.binstall.sh
+++ profile.d/binstall.d/create-tauri-app.cargo.binstall.sh
@@ -2,23 +2,29 @@
 set -Eeuo pipefail
 source $(u.here)/binstalld.lib.sh
 
-_kind="$(path.basename.part "$0" 1)"
-_pkg="$(path.basename "$(realpath -Lm "$0")")"
-_cmd="cargo-${_pkg}"
+main() (
+    local _kind=$(path.basename.part "$0" 1)
+    local -a _pkgs=( $(path.basename "$(realpath -Lm "$0")") )
+    local -a _cmds=( ${_kind}-$(path.basename "$(realpath -Lm "$0")") )
 
-_os_release=$(os-release.id)
-case ${_os_release} in
-    fedora) dnf install webkit2gtk4.1-devel openssl-devel curl wget file libappindicator-gtk3-devel librsvg2-devel ;;
-    ubuntu) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
-    *) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
-esac
+    _os_release=$(os-release.id)
+    case ${_os_release} in
+        fedora) binstall.dnf --pkg=webkit2gtk4.1-devel --pkg=openssl-devel --pkg=curl --pkg=wget --pkg=file --pkg=libappindicator-gtk3-devel --pkg=librsvg2-devel ;;
+        ubuntu) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
+        *) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
+    esac
+    
+    binstalld.${_kind} \
+                       $(u.switches pkg ${_pkgs[@]}) \
+                       $(u.switches cmd ${_cmds[@]}) \
+                       "$@"
+    # postinstall here
+    path.alias ${_cmd[0]} tauri
+)
 
-binstalld.dispatch --kind=""${_kind}" \
-                   --pkg="${_pkg}" \
-                   --cmd="${_cmd}" \
-                   --version "^2.0.0" "$@"
+main "$@"
 
-path.alias ${_cmd} tauri
+
 
 
 
