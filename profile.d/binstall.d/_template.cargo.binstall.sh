6f6e8e7f8711f84af6f6c7006c8313c3172c2584
Mike Carifio <mike@carif.io>
2025-01-02 01:06:36 -0500

    spell out installing prereqs

--- profile.d/binstall.d/_template.cargo.binstall.sh
+++ profile.d/binstall.d/_template.cargo.binstall.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-source $(u.here)/binstalld.lib.sh
+source $(u.here)/../$(path.basename.part "$0" 2).source.sh
 
 main() (
     local _kind=$(path.basename.part "$0" 1)
     local -a _pkgs=( $(path.basename "$0") )
     local -a _cmds=()
 
-    # prerequisites here
-    # local _os_release=$(os-release.id)
-    # case ${_os_release} in
-    #     fedora) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
-    #     ubuntu) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
-    #     *) >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
-    # esac
+    _os_release=$(os-release.id)
+    case ${_os_release} in
+        fedora) ;; # binstall.dnf --pkg=... ;;
+        ubuntu) ;; # binstall.apt --pkg=... ;;
+        *) ;; # >&2 echo "${_os_release} prerequisites for ${_pkg} needed? Continuing..." ;;
+    esac
 
-    binstall.${_kind} \
+    binstall.${_kind:-cargo} \
                        $(u.switches pkg ${_pkgs[@]}) \
                        $(u.switches cmd ${_cmds[@]}) \
                        "$@"
-    # postinstall here    
+    # postinstall here
+    true
 )
 
 main "$@"
