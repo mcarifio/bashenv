26607571efac5295b26065507f0782c1eee60da2
Mike Carifio <mike@carif.io>
2025-01-07 01:26:22 -0500

    Added starship https://starship.rs/guide

---
+++ profile.d/binstall.d/starship+starship.eget.binstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+source $(u.here)/../$(path.basename.part $0 2).source.sh
+# --pkg=${_owner}+${_project} [--cmd=]* --asset=.tar.gz --file='*'
+binstall.$(path.basename.part $0 1) \
+         --pkg=$(path.basename "$0") \
+         --asset=gnu.tar.gz \
+         --all \
+         "$@"
+# post install
+
