ccc551532548bc05c6bc22de4598528d7c23d486
Mike Carifio <mcarifio@ciq.com>
2025-01-17 13:14:36 -0500

    Add some comments

--- profile.d/binstall.d/_all.apt.binstall.sh
+++ profile.d/binstall.d/_all.apt.binstall.sh
@@ -2,5 +2,9 @@
 set -Eeuo pipefail
 source $(u.here)/../$(path.basename.part $0 2).source.sh
 # [--uri=]* [--suite=]* [--component=]* [_components=]* [--signed-by=]* [--pkg=]* [--check] [--cmd=*]
-binstall.$(path.basename.part $0 1) $(u.switches pkg $(< $0.list)) "$@"
+
+# _all.merged.binstall.sh and _all.apt.binstall.sh.list contain the list of packages to install 
+binstall.$(path.basename.part $0 1) \
+         $(u.switches pkg $(< ${0//.$(path.basename.part $0 1)./.merged.}) $(< $0.list)) \
+         "$@"
 # post install
--- profile.d/binstall.d/_all.dnf.binstall.sh
+++ profile.d/binstall.d/_all.dnf.binstall.sh
@@ -2,10 +2,10 @@
 set -Eeuo pipefail
 source $(u.here)/../$(path.basename.part $0 2).source.sh
 # [--import=]* [--repo=]* [--copr=]* [--pkg=]* [--cmd=]*
+
+# _all.merged.binstall.sh and _all.dnf.binstall.sh.list contain the list of packages to install 
 binstall.$(path.basename.part $0 1) \
-         $(u.switches pkg tree curl emacs doas keepassx plocate thunderbird terminator mtools dosfstools gparted \
-                      btrfs-{assistant,progs} chromium-browser etherwake eza foliate fzf gthumb hardinfo2 krita meld obs-studio okular pandoc \
-                      python-is-python3 ripgrep solaar tldr torbrowser-launcher vlc xonsh zoxide ) \
+         $(u.switches pkg $(< ${0//.$(path.basename.part $0 1)./.merged.}) $(< $0.list)) \
          "$@"
 # post install
 
