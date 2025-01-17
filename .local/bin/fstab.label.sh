6e46d1b22fbb55e9b2eca9a8e37a863955355bec
Mike Carifio <mcarifio@ciq.com>
2025-01-17 10:17:20 -0500

    fstab.label.sh will generate an /etc/fstab entry to mount all btrfs partitions with a (shared) file label.

--- .local/bin/fstab.label.sh
+++ .local/bin/fstab.label.sh
@@ -2,39 +2,74 @@
 set -Eeuo pipefail; shopt -s nullglob
 [[ "$0" = */bashdb ]] && shift
 
+# TODO mike@carif.io: do a udisk2 alternative
+
 main() (
-    local _label="${1:?"${FUNCNAME} expecting a label"}"
-    local _target=${2:-/tmp/fstab+${_label}}
+    : '${_label} [${_mountpoint} [${_target}]] ## create an /etc/fstab mount on ${_mountpoint} for all btrfs filesystems labeled ${_label}'
+    local _label="${1:?"${FUNCNAME}@${LINENO} expecting a label"}" ## label to search for, e.g. 'store'
+    local _root="/"
+    local _mountpoint="${2:-${_root}${_label}}" ## the mountpoint created, default /${_label}
+    local _target=${3:-/etc/fstab} ## the output. set this to something like /tmp/fstab+${_label} to inspect the result first
+    local _working="/tmp/$(basename $0)/fstab+${_label}" ## the intermediate result which findmnt --verify will inspect first
 
-    btrfs filesystem show ${_label} || u.warn "${FUNCNAME} no btrfs filesystem labeled '${_label}'"
+    # Create the pathname to ${_working} if needed
+    mkdir -p $(dirname ${_working}) &> /dev/null || true
+    
+    # Are there any btrfs filesystems actually labeled ${_label}?
+    btrfs filesystem show ${_label} || u.warn "${FUNCNAME}@${LINENO} no btrfs filesystem labeled '${_label}', continuing..."
 
-    cat /etc/fstab - <<EOF > ${_target}
+    # create the mount options
+    local -i _subvolid=$(btrfs subvolume get-default ${_root} | cut -d' ' -f2) ## usually 5
+    local _options="defaults,nofail,degraded,discard=async,space_cache=v2,ssd,autodefrag,subvolid=${_subvolid:-5},subvol=${_root}"
 
-# ${USER} $(date --iso) mount all disks with btrfs file label '${_label}' on /${_label} with some fault tolerance
-# as root:
-# mkdir /${_label}
+    if findmnt --fstab --timeout 3000 --mountpoint "${_mountpoint}"; then
+        _target=${_working}.generated
+        >&2 echo "${FUNCNAME}@${LINENO} /etc/fstab already has a mountpoint ${_mountpoint}."
+        >&2 echo "${FUNCNAME}@${LINENO} You must merge ${_target} and /etc/fstab manually"
+    fi
+    
+    
+    cat /etc/fstab - <<EOF > ${_working}
+
+# $0 ${USER} $(date --iso)
+# mount all partitions formatted btrfs and labeled ${_label} on ${_mountpoint} (with some fault tolerance)
+
+# To approximate $0 manually as root:
+# mkdir -pv ${_mountpoint}
+# emacs /etc/fstab # add entry
 # systemctl daemon-reload
 # findmnt --verify # check syntax
 # findmnt --fstab # what should happen next
-# mount /${_label}
-# mount -l --type=btrfs | grep ${_label} # check options
+# mount ${_mountpoint}
+# findmnt ${_mountpoint}
 # btrfs filesystems show ${_label} # find disks by label and enumerate them
 
-# noauto for rebooting and mounting manually: 'sudo mount /${_label}'. Note that udisk2 will also "recognize" store.
-# LABEL=${_label}                                            /${_label}          btrfs   defaults,nofail,degraded,noauto,discard,autodefrag 0 1
-# automount on boot
-LABEL=${_label}                                            /${_label}          btrfs   defaults,nofail,degraded,discard,autodefrag 0 1
+# manually as root without modifying fstab:
+# install -d -o ${USER} -g ${USER} ${_mountpoint}
+# mount -o ${_options} LABEL=${_label} ${_mountpoint}
+
+
+# noauto for rebooting and mounting manually: 'sudo mount ${_mountpoint}'. Note that udisk2 will also "recognize" store.
+# LABEL=${_label}                                            ${_mountpoint}          btrfs   ${_options},noauto 0 1
+# automount ${_mountpoint} on boot
+LABEL=${_label}                                            ${_mountpoint}          btrfs   ${_options} 0 1
 
 EOF
 
-    >&2 echo "created '${_target}' ## for your inspection"
-    >&2 echo "sudo cp -v --backup=numbered ${_target} /etc/fstab && sudo mkdir /${_label} && sudo systemctl daemon-reload && mount -l --type=btrfs"
-    echo ${_target}
+    (set -x; LIBMOUNT_FSTAB="${_working}" sudo findmnt --verbose --verify) || return $(u.error "${FUNCNAME}@${LINENO}: ${_working} did not verify")
+    sudo cp --update --verbose --backup=numbered "${_working}" "${_target}" || true
+    >&2 echo "Augmented fstab: ${_target}"
+    if [[ /etc/fstab = "${_target}" ]]; then
+        [[ -d "${_mountpoint}" ]] && >&2 "mountpoint '${_mountpoint}' exists, continuing..." || sudo install --directory --owner=${USER} --group=${USER} "${_mountpoint}"
+        sudo systemctl daemon-reload
+        sudo umount --verbose ${_mountpoint} && sudo mount --verbose ${_mountpoint}
+        findmnt "${_mountpoint}" || return $(u.error "${FUNCNAME}@${LINENO}: '${_mountpoint}' not mounted? Check /etc/fstab.")
+    fi    
 )
 
-main "$@"
 
+# for testing:
+main store "/store" /tmp/fstab+store
 
-
-
-
+# for real:
+# main "$@"
