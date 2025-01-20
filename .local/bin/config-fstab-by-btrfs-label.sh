eac5a8c1e17f846458becff90cee230c9e361e3c
Mike Carifio <mike@carif.io>
2025-01-20 13:00:07 -0500

    config-fstab-by-btrfs-label.sh uses meld to let operator approve /etc/fstab modifications

--- .local/bin/config-fstab-by-btrfs-label.sh
+++ .local/bin/config-fstab-by-btrfs-label.sh
@@ -9,14 +9,14 @@ main() (
     local _label="${1:?"${FUNCNAME}@${LINENO} expecting a label"}" ## label to search for, e.g. 'store'
     local _root="/"
     local _mountpoint="${2:-${_root}${_label}}" ## the mountpoint created, default /${_label}
-    local _target=${3:-/etc/fstab} ## the output. set this to something like /tmp/fstab+${_label} to inspect the result first
+    local _target=${3:-/tmp/fstab+{_label}} ## the output. set this to something like /tmp/fstab+${_label} to inspect the result first
     local _working="/tmp/$(basename $0)/fstab+${_label}" ## the intermediate result which findmnt --verify will inspect first
 
     # Create the pathname to ${_working} if needed
     mkdir -p $(dirname ${_working}) &> /dev/null || true
     
     # Are there any btrfs filesystems actually labeled ${_label}?
-    btrfs filesystem show ${_label} || u.warn "${FUNCNAME}@${LINENO} no btrfs filesystem labeled '${_label}', continuing..."
+    btrfs filesystem show ${_label} || u.warn "${FUNCNAME}@${BASH_LINENO}: no btrfs filesystem labeled '${_label}', continuing..."
 
     # create the mount options
     local -i _subvolid=$(btrfs subvolume get-default ${_root} | cut -d' ' -f2) ## usually 5
@@ -24,8 +24,8 @@ main() (
 
     if findmnt --fstab --timeout 3000 --mountpoint "${_mountpoint}"; then
         _target=${_working}.generated
-        >&2 echo "${FUNCNAME}@${LINENO} /etc/fstab already has a mountpoint ${_mountpoint}."
-        >&2 echo "${FUNCNAME}@${LINENO} You must merge ${_target} and /etc/fstab manually"
+        >&2 echo "${FUNCNAME}@${BASH_LINENO}: /etc/fstab already has a mountpoint ${_mountpoint}."
+        >&2 echo "${FUNCNAME}@${BASH_LINENO}: You must merge ${_target} and /etc/fstab manually"
     fi
     
     
@@ -56,20 +56,22 @@ LABEL=${_label}                                            ${_mountpoint}       
 
 EOF
 
-    (set -x; LIBMOUNT_FSTAB="${_working}" sudo findmnt --verbose --verify) || return $(u.error "${FUNCNAME}@${LINENO}: ${_working} did not verify")
+    (set -x; LIBMOUNT_FSTAB="${_working}" sudo findmnt --verbose --verify) || return $(u.error "${FUNCNAME}@${BASH_LINENO}: ${_working} did not verify")
     sudo cp --update --verbose --backup=numbered "${_working}" "${_target}" || true
-    >&2 echo "Augmented fstab: ${_target}"
-    if [[ /etc/fstab = "${_target}" ]]; then
-        [[ -d "${_mountpoint}" ]] && >&2 "mountpoint '${_mountpoint}' exists, continuing..." || sudo install --directory --owner=${USER} --group=${USER} "${_mountpoint}"
+    [[ -d "${_mountpoint}" ]] && >&2 "mountpoint '${_mountpoint}' exists, continuing..." || \
+            sudo install --directory --owner=${USER} --group=${USER} "${_mountpoint}"
+    cp /etc/fstab /tmp/fstab
+    sudo meld ${_target} /etc/fstab
+    if diff --ignore-tab-expansion --ignore-trailing-space --ignore-space-change --ignore-blank-lines /{tmp,etc}/fstab &> /dev/null; then
+        >&2 echo "/tmp/fstab is still different from /etc/fstab"
+    else
         sudo systemctl daemon-reload
         sudo umount --verbose ${_mountpoint} && sudo mount --verbose ${_mountpoint}
         findmnt "${_mountpoint}" || return $(u.error "${FUNCNAME}@${LINENO}: '${_mountpoint}' not mounted? Check /etc/fstab.")
-    fi    
+    fi
 )
 
 
 # for testing:
-main store "/store" /tmp/fstab+store
+main store "/store"
 
-# for real:
-# main "$@"
