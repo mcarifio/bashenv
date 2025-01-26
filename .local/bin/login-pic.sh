b325bb52daf57c5c6cc22196709da949ef4e82fb
Mike Carifio <mike@carif.io>
2025-01-26 00:31:38 -0500

    create a user gnome image with the hostname to make it clearer what machine you are logging into

---
+++ .local/bin/login-pic.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail; shopt -s nullglob
+
+main() (
+    local _png="${1:-${HOME}/Pictures/$(hostname).png}"
+    local _tmp="/tmp/$(basename ${_png})"
+    convert -size 512x512 -background white -fill black -gravity center -pointsize 128 label:"$(hostname)" "${_tmp}"
+    cp -v --backup=numbered "${_tmp}" "${_png}"
+    gsettings set org.gnome.desktop.account picture-uri "file://$(realpath ${_png})" || return $(u.error "${_png} needs manual setup")
+)
+
+
+main "$@"
+
