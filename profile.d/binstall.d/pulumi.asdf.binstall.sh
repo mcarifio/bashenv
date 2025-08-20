687baab29d76442a094383f3ab098b41785d101c
Mike Carifio <mike@carif.io>
2025-08-20 15:16:07 -0400

    install pulumi w/asdf

---
+++ profile.d/binstall.d/pulumi.asdf.binstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+source $(u.here)/../$(path.basename.part $0 2).source.sh
+# --url= --version= [--pkg=] [--cmd=]* [--url=plugin-url]
+binstall.$(path.basename.part $0 1) \
+         --pkg=$(path.basename "$0") \
+         --url=https://github.com/canha/asdf-pulumi.git \
+         "$@"
+# post install
